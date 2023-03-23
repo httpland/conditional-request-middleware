96306e49e07f2281a5ad4b917d5a884dd364734f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-23 21:57:35 +0900

    chore: fix test case

--- utils_test.ts
+++ utils_test.ts
@@ -18,13 +18,15 @@ describe("toPriority", () => {
   it("should return priority number", () => {
     const table: [string, number][] = [
       [ConditionalHeader.IfMatch, 0],
-      [ConditionalHeader.IfNoneMatch, 1],
-      [ConditionalHeader.IfModifiedSince, 2],
-      [ConditionalHeader.IfUnmodifiedSince, 3],
+      [ConditionalHeader.IfUnmodifiedSince, 1],
+      [ConditionalHeader.IfNoneMatch, 2],
+      [ConditionalHeader.IfModifiedSince, 3],
+      [ConditionalHeader.IfRange, 4],
       ["If-Match", 0],
-      ["If-None-Match", 1],
-      ["If-Modified-Since", 2],
-      ["If-Unmodified-Since", 3],
+      ["If-Unmodified-Since", 1],
+      ["If-None-Match", 2],
+      ["If-Modified-Since", 3],
+      ["If-Range", 4],
       ["unknown", Infinity],
     ];
 
@@ -43,26 +45,44 @@ describe("ascendPreconditionHeader", () => {
       [ConditionalHeader.IfMatch, ConditionalHeader.IfRange, -1],
       [ConditionalHeader.IfMatch, ConditionalHeader.IfUnmodifiedSince, -1],
       [ConditionalHeader.IfMatch, "unknown", -1],
-      [ConditionalHeader.IfNoneMatch, ConditionalHeader.IfNoneMatch, 0],
-      [ConditionalHeader.IfNoneMatch, ConditionalHeader.IfModifiedSince, -1],
-      [ConditionalHeader.IfNoneMatch, ConditionalHeader.IfRange, -1],
-      [ConditionalHeader.IfNoneMatch, ConditionalHeader.IfUnmodifiedSince, -1],
-      [ConditionalHeader.IfNoneMatch, "unknown", -1],
-      [ConditionalHeader.IfModifiedSince, ConditionalHeader.IfModifiedSince, 0],
-
-      [ConditionalHeader.IfModifiedSince, ConditionalHeader.IfRange, -1],
-      [
-        ConditionalHeader.IfModifiedSince,
-        ConditionalHeader.IfUnmodifiedSince,
-        -1,
-      ],
-      [ConditionalHeader.IfModifiedSince, "unknown", -1],
       [
         ConditionalHeader.IfUnmodifiedSince,
         ConditionalHeader.IfUnmodifiedSince,
         0,
       ],
+      [
+        ConditionalHeader.IfUnmodifiedSince,
+        ConditionalHeader.IfModifiedSince,
+        -1,
+      ],
+      [ConditionalHeader.IfUnmodifiedSince, ConditionalHeader.IfNoneMatch, -1],
+      [ConditionalHeader.IfUnmodifiedSince, ConditionalHeader.IfRange, -1],
       [ConditionalHeader.IfUnmodifiedSince, "unknown", -1],
+      [
+        ConditionalHeader.IfNoneMatch,
+        ConditionalHeader.IfNoneMatch,
+        0,
+      ],
+      [
+        ConditionalHeader.IfNoneMatch,
+        ConditionalHeader.IfModifiedSince,
+        -1,
+      ],
+      [ConditionalHeader.IfNoneMatch, ConditionalHeader.IfRange, -1],
+      [ConditionalHeader.IfNoneMatch, "unknown", -1],
+      [
+        ConditionalHeader.IfModifiedSince,
+        ConditionalHeader.IfModifiedSince,
+        0,
+      ],
+      [
+        ConditionalHeader.IfModifiedSince,
+        ConditionalHeader.IfRange,
+        -1,
+      ],
+      [ConditionalHeader.IfModifiedSince, "unknown", -1],
+      [ConditionalHeader.IfRange, ConditionalHeader.IfRange, 0],
+      [ConditionalHeader.IfRange, "unknown", -1],
     ];
 
     table.forEach(([left, right, expected]) => {
@@ -93,7 +113,7 @@ describe("applyPrecondition", () => {
 
   it("should return void if the evaluate return true", async () => {
     const evaluate = spy(() => true);
-    const respond = spy(() => new Response());
+    const respond = spy(() => {});
 
     const result = await applyPrecondition(
       new Request("test:", { headers: { "unknown": "" } }),
@@ -106,7 +126,7 @@ describe("applyPrecondition", () => {
     );
 
     assertSpyCalls(evaluate, 1);
-    assertSpyCalls(respond, 0);
+    assertSpyCalls(respond, 1);
     assert(!result);
   });
 
