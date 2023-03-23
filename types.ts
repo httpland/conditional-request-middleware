2e4a850166e7695bcb8c97cadb9205c9ce0e2eb6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-23 21:58:48 +0900

    docs(types): add code comment

--- types.ts
+++ types.ts
@@ -1,17 +1,26 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
+/** Precondition API. */
 export interface Precondition {
+  /** Precondition header field name. */
   readonly field: string;
 
+  /** Definition of precondition evaluation.
+   * If return value is void, it represents ignore this precondition.
+   */
   evaluate(
     request: Request,
     selectedRepresentation: Response,
   ): boolean | void | Promise<boolean | void>;
 
+  /** Called after {@link Precondition.evaluate}.
+   * If return response, it must not perform the requested method.
+   * If return value is void, it represents ignore this precondition.
+   */
   respond(
     request: Request,
     selectedRepresentation: Response,
     result: boolean,
-  ): Response | void;
+  ): Response | void | Promise<Response | void>;
 }
