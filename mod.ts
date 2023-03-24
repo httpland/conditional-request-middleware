6d43a63a504514b2cf34d232d214d6aff81cb76d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-24 16:51:19 +0900

    feat(mod): export types

--- mod.ts
+++ mod.ts
@@ -1,9 +1,11 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { conditionalRequest } from "./middleware.ts";
+export { conditionalRequest, type Options } from "./middleware.ts";
 export { IfNoneMatch } from "./preconditions/if_none_match.ts";
 export { IfMatch } from "./preconditions/if_match.ts";
 export { IfModifiedSince } from "./preconditions/if_modified_since.ts";
 export { IfUnmodifiedSince } from "./preconditions/if_unmodified_since.ts";
 export { IfRange } from "./preconditions/if_range.ts";
+export { type Handler, type Middleware } from "./deps.ts";
+export { type Precondition } from "./types.ts";
