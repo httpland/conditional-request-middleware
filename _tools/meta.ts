192e84f24f5cad9ccfc5bea9f2876cd92898a9d6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-05-02 12:34:36 +0900

    feat(deps): update deps version

--- _tools/meta.ts
+++ _tools/meta.ts
@@ -4,7 +4,7 @@ export const makeOptions = (version: string): BuildOptions => ({
   test: false,
   shims: {},
   compilerOptions: {
-    lib: ["esnext", "dom"],
+    lib: ["esnext", "dom", "dom.iterable"],
   },
   typeCheck: true,
   entryPoints: ["./mod.ts"],
@@ -44,44 +44,49 @@ export const makeOptions = (version: string): BuildOptions => ({
   },
   packageManager: "pnpm",
   mappings: {
-    "https://deno.land/x/isx@1.1.1/is_boolean.ts": {
+    "https://deno.land/x/isx@1.3.1/is_boolean.ts": {
       name: "@miyauci/isx",
-      version: "1.1.1",
-      subPath: "is_boolean",
+      version: "1.3.1",
+      subPath: "is_boolean.js",
     },
-    "https://deno.land/x/isx@1.1.1/is_null.ts": {
+    "https://deno.land/x/isx@1.3.1/is_null.ts": {
       name: "@miyauci/isx",
-      version: "1.1.1",
-      subPath: "is_null",
+      version: "1.3.1",
+      subPath: "is_null.js",
     },
-    "https://deno.land/x/isx@1.1.1/is_string.ts": {
+    "https://deno.land/x/isx@1.3.1/is_string.ts": {
       name: "@miyauci/isx",
-      version: "1.1.1",
-      subPath: "is_string",
+      version: "1.3.1",
+      subPath: "is_string.js",
     },
-    "https://deno.land/x/isx@1.1.1/is_number.ts": {
+    "https://deno.land/x/isx@1.3.1/is_number.ts": {
       name: "@miyauci/isx",
-      version: "1.1.1",
-      subPath: "is_number",
+      version: "1.3.1",
+      subPath: "is_number.js",
     },
-    "https://deno.land/x/isx@1.1.1/number/is_negative_number.ts": {
+    "https://deno.land/x/isx@1.3.1/number/is_negative_number.ts": {
       name: "@miyauci/isx",
-      version: "1.1.1",
-      subPath: "number/is_negative_number",
+      version: "1.3.1",
+      subPath: "number/is_negative_number.js",
     },
     "https://deno.land/x/http_middleware@1.0.0/mod.ts": {
       name: "@httpland/http-middleware",
       version: "1.0.0",
     },
-    "https://deno.land/x/http_utils@1.0.0/header.ts": {
+    "https://deno.land/x/http_utils@1.2.0/header.ts": {
       name: "@httpland/http-utils",
-      version: "1.0.0",
+      version: "1.2.0",
       subPath: "header.js",
     },
-    "https://deno.land/x/http_utils@1.0.0/method.ts": {
+    "https://deno.land/x/http_utils@1.2.0/method.ts": {
       name: "@httpland/http-utils",
-      version: "1.0.0",
+      version: "1.2.0",
       subPath: "method.js",
+    },
+    "https://deno.land/x/http_utils@1.2.0/list.ts": {
+      name: "@httpland/http-utils",
+      version: "1.2.0",
+      subPath: "list.js",
     },
     "https://deno.land/x/result_js@1.0.0/mod.ts": {
       name: "@miyauci/result",
@@ -91,19 +96,14 @@ export const makeOptions = (version: string): BuildOptions => ({
       name: "@httpland/range-request-middleware",
       version: "1.2.0",
     },
-    "https://deno.land/x/accept_ranges_parser@1.0.0/mod.ts": {
+    "https://deno.land/x/accept_ranges_parser@1.0.1/mod.ts": {
       name: "@httpland/accept-ranges-parser",
-      version: "1.0.0",
+      version: "1.0.1",
     },
-    "https://deno.land/x/prelude_js@1.0.0/to_lower_case.ts": {
+    "https://deno.land/x/prelude_js@1.2.0/to_lower_case.ts": {
       name: "@miyauci/prelude",
-      version: "1.0.0",
-      subPath: "to_lower_case",
-    },
-    "https://deno.land/x/http_utils@1.2.0/list.ts": {
-      name: "@httpland/http-utils",
       version: "1.2.0",
-      subPath: "list.js",
+      subPath: "to_lower_case.js",
     },
   },
 });
--- deps.ts
+++ deps.ts
@@ -7,12 +7,12 @@ export {
   type SuccessfulStatus,
 } from "https://deno.land/std@0.181.0/http/http_status.ts";
 export { distinct } from "https://deno.land/std@0.181.0/collections/distinct.ts";
-export { isBoolean } from "https://deno.land/x/isx@1.1.1/is_boolean.ts";
-export { isNull } from "https://deno.land/x/isx@1.1.1/is_null.ts";
-export { isString } from "https://deno.land/x/isx@1.1.1/is_string.ts";
-export { isNumber } from "https://deno.land/x/isx@1.1.1/is_number.ts";
-export { isNegativeNumber } from "https://deno.land/x/isx@1.1.1/number/is_negative_number.ts";
-export { toLowerCase } from "https://deno.land/x/prelude_js@1.0.0/to_lower_case.ts";
+export { isBoolean } from "https://deno.land/x/isx@1.3.1/is_boolean.ts";
+export { isNull } from "https://deno.land/x/isx@1.3.1/is_null.ts";
+export { isString } from "https://deno.land/x/isx@1.3.1/is_string.ts";
+export { isNumber } from "https://deno.land/x/isx@1.3.1/is_number.ts";
+export { isNegativeNumber } from "https://deno.land/x/isx@1.3.1/number/is_negative_number.ts";
+export { toLowerCase } from "https://deno.land/x/prelude_js@1.2.0/to_lower_case.ts";
 export {
   type Handler,
   type Middleware,
@@ -24,11 +24,12 @@ export {
   isRepresentationHeader,
   RangeHeader,
   RepresentationHeader,
-} from "https://deno.land/x/http_utils@1.0.0/header.ts";
+} from "https://deno.land/x/http_utils@1.2.0/header.ts";
 export {
   isRetrieveMethod,
   Method,
-} from "https://deno.land/x/http_utils@1.0.0/method.ts";
+} from "https://deno.land/x/http_utils@1.2.0/method.ts";
+export { parseListFields } from "https://deno.land/x/http_utils@1.2.0/list.ts";
 export {
   compareStrong,
   compareWeak,
@@ -38,7 +39,7 @@ export {
 } from "https://deno.land/x/etag_parser@1.1.0/mod.ts";
 export { isErr, unsafe } from "https://deno.land/x/result_js@1.0.0/mod.ts";
 export { ascend } from "https://deno.land/std@0.181.0/collections/_comparators.ts";
-export { withContentRange } from "https://deno.land/x/range_request_middleware@1.2.0/transform.ts";
+export { withContentRange } from "https://deno.land/x/range_request_middleware@1.3.0/transform.ts";
 export {
   type BytesContext,
   BytesRange,
@@ -55,8 +56,7 @@ export { default as parseHttpDate } from "https://esm.sh/http-dates@1.2.0";
 export {
   parseAcceptRanges,
   type Token,
-} from "https://deno.land/x/accept_ranges_parser@1.0.0/mod.ts";
-export { parseListFields } from "https://deno.land/x/http_utils@1.2.0/list.ts";
+} from "https://deno.land/x/accept_ranges_parser@1.0.1/mod.ts";
 
 export function not<T extends readonly unknown[]>(
   fn: (...args: T) => boolean,
